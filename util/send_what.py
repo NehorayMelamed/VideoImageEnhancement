e255f4ccfe7ceac2d702160137e866e44e8c9d3b
nehoray <NEHORAYMELAMED8787@GMAIL.COM>
2024-07-06 15:41:21 +0300

    Remove .pyc files from the repository

--- util/send_what.py
+++
@@ -1,5 +0,0 @@
-import pywhatkit
-BASE_ISRAEL = "+972"
-NOYA = "538279402"
-for i in range(100):
-    pywhatkit.sendwhatmsg_instantly(phone_no=f"{BASE_ISRAEL}{NOYA}", message="מה את עושה נויה?", wait_time=4)
